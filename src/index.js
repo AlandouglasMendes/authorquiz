a10d5a7b00e27d40d93ae549d29e68ee21ac3d48
alandouglas <adgmendes@hotmail.com>
2020-04-16 21:38:11 -0300

    try

--- src/index.js
+++ src/index.js
@@ -11,6 +11,30 @@ const authors = [
   imageSource: 'Wikipedia commons',
   books: ['The Adventures of Huckleberry-Finn']
 },
+{
+  name: 'J. K. Rowling',
+  imageUrl: 'images/authors/rowling.jpg',
+  imageSource: 'Wikipedia commons',
+  books: ['Harry potter series']
+},
+{
+  name: 'Joseph Conrad',
+  imageUrl: 'images/authors/conrad.jpg',
+  imageSource: 'Wikipedia commons',
+  books: ['Heart of darkness']
+},
+{
+  name: 'Charles Dickens',
+  imageUrl: 'images/authors/dickens.jpg',
+  imageSource:'Wikipedia commons',
+  books: ['A tale of Two Cities']
+},
+{
+  name: 'Shakespeare',
+  imageUrl: 'images/authors/shakespeare.jpg',
+  imageSource: 'Wikipedia commons',
+  books: 'Macbeth'
+}
 ];
 
 const state = {
