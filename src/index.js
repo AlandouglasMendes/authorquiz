39ef50cde843afc0a2139f67cb6d96e45611dd85
alandouglas <adgmendes@hotmail.com>
2020-04-16 21:38:35 -0300

    try

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import AuthorQuiz from './AuthorQuiz';
 const authors = [
   {
   name: 'Mark Twain',
-  imageUrl: 'images/authors/mark.jpg',
+  imageUrl: 'images/authors/twain.jpg',
   imageSource: 'Wikipedia commons',
   books: ['The Adventures of Huckleberry-Finn']
 },
